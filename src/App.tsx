0dcf1ac9c64aa036a81e7a346e5c9b0e6f964aad
Giuseppe Arimatea <giuseppearimatea@gmail.com>
2021-01-06 20:12:16 +0100

    feat(general): api fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { FC, useEffect, useState } from "react";
 import "./App.css";
 import { Header } from "./components/header";
 
-export const App = () => {
+interface Todo {
+  completed: boolean;
+  id: number;
+  title: string;
+  userId: number;
+}
+
+export const App: FC = (): JSX.Element => {
+  const [result, setResult] = useState<Todo[]>([]);
+
+  const fetchURL = "https://jsonplaceholder.typicode.com/todos";
+  const getData = () => fetch(fetchURL).then((res) => res.json());
+
+  useEffect(() => {
+    getData().then((data) => setResult(data));
+  }, []);
+
   return (
     <>
       <Header />
+      <div>
+        {result?.map((item) => (
+          <li key={item.id}>
+            {item.title},{item.userId}
+          </li>
+        ))}
+      </div>
     </>
   );
 };
